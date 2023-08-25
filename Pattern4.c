293b886884c89ae3ba52ac1dbdef7f29c5b3aaa7
Abhishek Pawar <117835961+AbhishekPawar27@users.noreply.github.com>
2023-08-25 19:06:40 +0530

    Add files via upload

---
+++ Pattern4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,n;
+	printf("Enter the number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	   for(j=1;j<=n;j++)
+	   {
+	   	if(i%2==0)
+	   	{
+	   		printf("%c",64+i);
+		   }
+		 else
+		 {
+		 	printf("%c",96+i);
+		   }  
+	   }
+	   printf("\n");
+	}
+	getch();
+}
